883e2501c0cb8bea280001bfef3ca22467f8025f
ArsenioDraco <voynovarseny@gmail.com>
2025-12-02 21:17:53 +0300

    Update script.ts

--- script.ts
+++ script.ts
@@ -36,3 +36,11 @@ async function loadIngredients(): Promise<Ingredient[]> {
     return FALLBACK_INGREDIENTS;
   }
 }
+function seededRandom(seed: number) {
+  let s = seed % 2147483647;
+  if (s <= 0) s += 2147483646;
+  return () => {
+    s = (s * 16807) % 2147483647;
+    return (s - 1) / 2147483646;
+  };
+}
